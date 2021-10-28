61e86ebb927f386a9ae2ba372e8b3d3ac03b57c5
ten-point-nine <58998445+ten-point-nine@users.noreply.github.com>
2021-10-27 20:38:43 -0500

    Arduino 3.4.15 Fixed bug in initialization command
    {"INIT":1234} was incorrectly implemented as IS_FIXED instead of IS_INT16

--- Software/Arduino/freETarget/freETarget.h
+++ Software/Arduino/freETarget/freETarget.h
@@ -13,7 +13,7 @@
 #include "esp-01.h"
 #include "json.h"
 
-#define SOFTWARE_VERSION "\"3.04.14 October 18, 2021\""
+#define SOFTWARE_VERSION "\"3.04.15 October 23, 2021\""
 #define REV_100    100
 #define REV_210    210
 #define REV_220    2201001
