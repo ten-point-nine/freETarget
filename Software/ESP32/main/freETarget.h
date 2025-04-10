97d0de50aa838456e62bf40a1b82961c514c6e50
Allan Brown <58998445+ten-point-nine@users.noreply.github.com>
2025-04-09 19:53:37 -0500

    Deleted objects and recompiled

--- Software/ESP32/main/freETarget.h
+++ Software/ESP32/main/freETarget.h
@@ -13,7 +13,7 @@
 #include "freertos/task.h"
 #include "serial_io.h"
 
-#define SOFTWARE_VERSION "\"6.1.0 April 9, 2025\""
+#define SOFTWARE_VERSION "\"6.1.1 April 9, 2025\""
 #define _DONE_           "\r\nDone\r\n"
 #define _GREETING_       "CONNECTED" // Message to send on connection
 
