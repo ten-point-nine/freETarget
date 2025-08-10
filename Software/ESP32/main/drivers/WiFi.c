0a852b6d4637ed4fe0b0c84fc24a0e05268ab5ff
Allan Brown <58998445+ten-point-nine@users.noreply.github.com>
2025-08-10 18:34:24 -0500

    Added RSSI strength

--- Software/ESP32/main/drivers/WiFi.c
+++ Software/ESP32/main/drivers/WiFi.c
@@ -1025,3 +1025,85 @@ void WiFi_AP_loopback_test(void)
   WiFi_loopback_test();
   return;
 }
+
+/*****************************************************************************
+ *
+ * @function: WiFi_AP_scan_test
+ *
+ * @brief:    Find the APs in the area
+ *
+ * @return:   None
+ *
+ ****************************************************************************
+ *
+ * The function looks for APs that it can find and reports the SSID and
+ * the signal strenght.
+ *
+ * RSSIs closer to 0 are better
+ */
+
+void WiFi_AP_scan_test(void)
+{
+  wifi_ap_record_t ap_info[20]; // Space for 20 APs
+  uint16_t         ap_count = 20;
+  int              i, j;
+  char             str_c[SHORT_TEXT];
+
+  SEND(ALL, sprintf(_xs, "\r\nCloser to 0 is stronger");)
+
+  if ( esp_wifi_scan_start(NULL, true) != ESP_OK )
+  {
+    SEND(ALL, sprintf(_xs, "\r\nFailed to scan for APs");)
+    return;
+  }
+
+  if ( esp_wifi_scan_get_ap_records(&ap_count, ap_info) != ESP_OK )
+  {
+    SEND(ALL, sprintf(_xs, "\r\nFailed to get AP records");)
+    return;
+  }
+
+  for ( i = 0; i < ap_count; i++ )
+  {
+    SEND(ALL, sprintf(_xs, "\r\nAP[%d] SSID: %s,  RSSI:%d,  ", i, ap_info[i].ssid, ap_info[i].rssi);)
+
+    switch ( ap_info[i].authmode )
+    {
+      case WIFI_AUTH_OPEN:
+        SEND(ALL, sprintf(_xs, " Auth: Open");) break;
+      case WIFI_AUTH_WEP:
+        SEND(ALL, sprintf(_xs, " Auth: WEP");) break;
+      case WIFI_AUTH_WPA_PSK:
+        SEND(ALL, sprintf(_xs, " Auth: WPA_PSK");) break;
+      case WIFI_AUTH_WPA2_PSK:
+        SEND(ALL, sprintf(_xs, " Auth: WPA2_PSK");) break;
+      case WIFI_AUTH_WPA_WPA2_PSK:
+        SEND(ALL, sprintf(_xs, " Auth: WPA_WPA2_PSK");) break;
+      case WIFI_AUTH_WPA2_ENTERPRISE:
+        SEND(ALL, sprintf(_xs, " Auth: WPA2_ENTERPRISE");) break;
+      case WIFI_AUTH_WPA3_PSK:
+        SEND(ALL, sprintf(_xs, " Auth: WPA3_PSK");) break;
+      case WIFI_AUTH_WPA2_WPA3_PSK:
+        SEND(ALL, sprintf(_xs, " Auth: WPA2_WPA3_PSK");) break;
+      case WIFI_AUTH_WAPI_PSK:
+        SEND(ALL, sprintf(_xs, " Auth: WAPI_PSK");) break;
+      case WIFI_AUTH_MAX:
+        SEND(ALL, sprintf(_xs, " Auth: MAX");) break;
+
+      default:
+        SEND(ALL, sprintf(_xs, " Auth: Unknown");) break;
+    }
+
+    SEND(ALL, sprintf(_xs, "     {\"WIFI_SSID\":\"%s\", \"WIFI_PWD\":\"---\"}", ap_info[i].ssid);)
+  }
+
+  /*
+   * All done, return
+   */
+
+  WiFi_my_IP_address(str_c);
+  SEND(ALL, sprintf(_xs, "\r\n\r\nConnected to AP SSID:  \"%s\"", json_wifi_ssid);)
+  SEND(ALL, sprintf(_xs, "   IP: \"%s\"", str_c);)
+  SEND(ALL, sprintf(_xs, _DONE_);)
+  return;
+}
